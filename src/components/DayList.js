fad95bac91324485cbccf070aa27bc38db84277c
Lucas Sahota <106697693+lucasw4@users.noreply.github.com>
2023-01-26 23:45:27 -0800

    Implement DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,22 @@
 import React from "react";
+import DayListItem from "./DayListItem";
 
-const DayList = () => {
-  return <div>DayList</div>;
+const DayList = (props) => {
+  return (
+    <ul>
+      {props.days.map((day) => {
+        return (
+          <DayListItem
+            key={day.id}
+            name={day.name}
+            spots={day.spots}
+            selected={day.name === props.day}
+            setDay={day.setDay}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default DayList;
