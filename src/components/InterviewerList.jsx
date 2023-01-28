399edab44eba542a7f291ee638bfe0869c7ca64a
Lucas Sahota <lucaswade4@gmail.com>
2023-01-28 15:08:58 -0800

    Implement InterviewerList component to render all interviewers

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
-const InterviewerList = () => {
+const InterviewerList = (props) => {
   return (
-    <div>InterviewerList</div>
-  )
-}
+    <section className='interviewers'>
+      <h4 className='interviewers__header text--light'>Interviewer</h4>
+      <ul className='interviewers__list'>
+        {props.interviewers.map((i) => {
+          return (
+            <InterviewerListItem
+              key={i.id}
+              id={i.id}
+              avatar={i.avatar}
+              name={i.name}
+              selected={props.interviewer === i.id}
+              setInterviewer={props.setInterviewer}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
 
-export default InterviewerList
+export default InterviewerList;
