696fe2da85caa413473af67e39b28dfa90198a6a
Lucas Sahota <lucaswade4@gmail.com>
2023-02-25 17:35:47 -0800

    Update useVisualMode to use setHistory instead of editing history state directly

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,21 +2,23 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history] = useState([initial]);
+  const [history, setHistory] = useState([initial]);
 
   const transition = function (newMode, replace = false) {
     if (!replace) {
       setMode(newMode);
-      history.push(newMode);
+      setHistory((prev) => [...prev, newMode]);
     } else {
       setMode(newMode);
     }
   };
 
   const back = function () {
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+    if (history.length >= 1) {
+      let historyCopy = [...history];
+      historyCopy.pop();
+      setHistory(historyCopy);
+      setMode(historyCopy[historyCopy.length - 1]);
     }
   };
 
